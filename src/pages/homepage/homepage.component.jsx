619c10ad59e68da766bbaaac15a9c2bd5b88d594
waraphon <eepw0211@gmail.com>
2020-02-10 23:50:35 +0700

    add sessionStorage

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 import "./homepage.style.scss";
 
+import { NYT_API_KEY } from "../../Config";
 import { FetchApiStart } from "../../reducer/new-york-time/nyt-action";
 import {
   selectArticles,
@@ -11,6 +12,14 @@ import {
 import CardItem from "../../components/cardItem/cardItem.componens";
 import LoadingIcon from "../../components/loadingIcon/loadingIcon";
 import { sort, findImageUrl } from "./homepage.utill";
+
+function shouldRenderWarning(isDevmode = false) {
+  if (!isDevmode) return;
+
+  return NYT_API_KEY ? null : (
+    <div style={{ color: "red" }}>You do not set API-Key !!</div>
+  );
+}
 
 function shouldRenderCartItem(articles = [], history, sortBy) {
   if (!articles.length) return <div>Not have any articles.</div>;
@@ -32,9 +41,12 @@ function shouldRenderCartItem(articles = [], history, sortBy) {
 
 const initNewest = "newest";
 const initOldest = "oldest";
+const initalState = {
+  searchString: "",
+  sortBy: initNewest
+};
 function HomepageComponent() {
-  const [searchString, setSearchString] = useState("");
-  const [sortBy, setSortBy] = useState(initNewest);
+  const [state, setState] = useState(initalState);
   const isFirstRun = useRef(true);
   const history = useHistory();
   const dispatch = useDispatch();
@@ -48,14 +60,24 @@ function HomepageComponent() {
     if (isFirstRun.current) {
       isFirstRun.current = false;
       const sessionSearchString = window.sessionStorage.getItem("search");
-      setSearchString(sessionSearchString);
-      dispatch(FetchApiStart(sessionSearchString));
+      const sessionOption = window.sessionStorage.getItem("option");
+      setState({
+        searchString: sessionSearchString,
+        sortBy: sessionOption
+      });
+      if (sessionSearchString === state.searchString) {
+        dispatch(FetchApiStart(state.searchString));
+      }
       return;
     }
 
-    window.sessionStorage.setItem("search", searchString);
-    dispatch(FetchApiStart(searchString));
-  }, [dispatch, searchString]);
+    dispatch(FetchApiStart(state.searchString));
+  }, [dispatch, state.searchString]);
+
+  useEffect(() => {
+    window.sessionStorage.setItem("search", state.searchString);
+    window.sessionStorage.setItem("option", state.sortBy);
+  }, [state]);
 
   return (
     <div className='homepage'>
@@ -63,27 +85,32 @@ function HomepageComponent() {
         <i className='fa fa-search search-icon'></i>
         <input
           type='search'
-          onChange={e => setSearchString(e.target.value)}
-          value={searchString}
+          onChange={e => setState({ ...state, searchString: e.target.value })}
+          value={state.searchString}
         />
         <div className='tools select'>
           <label
-            className={`tools select ${sortBy === "newest" ? "active" : null}`}
-            onClick={() => setSortBy(initNewest)}>
+            className={`tools select ${
+              state.sortBy === "newest" ? "active" : null
+            }`}
+            onClick={() => setState({ ...state, sortBy: initNewest })}>
             Newest
           </label>
           <label
-            className={`tools select ${sortBy !== "newest" ? "active" : null}`}
-            onClick={() => setSortBy(initOldest)}>
+            className={`tools select ${
+              state.sortBy !== "newest" ? "active" : null
+            }`}
+            onClick={() => setState({ ...state, sortBy: initOldest })}>
             Oldest
           </label>
         </div>
       </div>
       <div className='items-container'>
+        {shouldRenderWarning(process.env.NODE_ENV === "development")}
         {isLoading ? (
           <LoadingIcon style={{ fontSize: "2rem", marginTop: "5rem" }} />
         ) : (
-          shouldRenderCartItem(articles, history, sortBy)
+          shouldRenderCartItem(articles, history, state.sortBy)
         )}
       </div>
     </div>
